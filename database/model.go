81b4c640269c40c6e462cc623de849e2c9a60ec2
liweiye <1101931365@qq.com>
2025-12-30 08:50:18 +0800

    model

--- database/model.go
+++ database/model.go
@@ -10,3 +10,14 @@ type BaseModel struct {
 	UpdatedAt time.Time `gorm:"column:updated_at;type:datetime;index;autoCreateTime;not null;comment:更新时间"`
 	DeletedAt time.Time `gorm:"column:deleted_at;type:datetime;default:null;comment:删除时间"`
 }
+
+type SimpleBaseModel struct {
+	ID        uint64    `gorm:"primaryKey;autoIncrement;column:id;comment:id"`
+	CreatedAt time.Time `gorm:"column:created_at;type:datetime;index;autoCreateTime;not null;comment:创建时间"`
+	UpdatedAt time.Time `gorm:"column:updated_at;type:datetime;index;autoCreateTime;not null;comment:更新时间"`
+}
+
+type TinyBaseModel struct {
+	ID        uint64    `gorm:"primaryKey;autoIncrement;column:id;comment:id"`
+	UpdatedAt time.Time `gorm:"column:updated_at;type:datetime;index;autoCreateTime;not null;comment:更新时间"`
+}
